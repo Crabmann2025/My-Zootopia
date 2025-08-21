5ab2fa06cbe340cd75fa208ae0cb39964423559c
Crabmann2025 <1990.c@gmx.de>
2025-08-21 15:45:33 +0200

    Refactor animal HTML serialization

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,35 +1,29 @@
 import json
 
-
 def load_data(dateipfad):
     """Load JSON-Data"""
     with open(dateipfad, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
-
 def get_name(animal):
     """Returns the name if available"""
     return animal.get("name")
 
-
 def get_diet(animal):
     """Returns nutrition if present"""
     return animal.get("characteristics", {}).get("diet")
-
 
 def get_location(animal):
     """Returns the first location, if any"""
     locations = animal.get("locations", [])
     return locations[0] if locations else None
 
-
 def get_type(animal):
     """Returns the type if any"""
     return animal.get("characteristics", {}).get("type")
 
-
-def get_animal_html(animal):
-    """Returns the information of an animal as an HTML string"""
+def serialize_animal(animal):
+    """Serialize a single animal as HTML"""
     output = '<li class="cards__item">\n'
 
     name = get_name(animal)
@@ -38,25 +32,25 @@ def get_animal_html(animal):
     typ = get_type(animal)
 
     if name:
-        output += f"Name: {name}<br/>\n"
+        output += f'<div class="card__title">{name}</div>\n'
+    output += '<p class="card__text">\n'
     if diet:
-        output += f"Diet: {diet}<br/>\n"
+        output += f'    <strong>Diet:</strong> {diet}<br/>\n'
     if location:
-        output += f"Location: {location}<br/>\n"
+        output += f'    <strong>Location:</strong> {location}<br/>\n'
     if typ:
-        output += f"Type: {typ}<br/>\n"
+        output += f'    <strong>Type:</strong> {typ}<br/>\n'
+    output += '</p>\n'
+    output += '</li>\n'
 
-    output += '</li>\n'  # End of list item
     return output
-
 
 def get_all_animals_html(data):
     """Iterates over all animals and returns a complete HTML string"""
     output = ''
     for animal in data:
-        output += get_animal_html(animal)
+        output += serialize_animal(animal)
     return output
-
 
 if __name__ == "__main__":
     animals = load_data("animals_data.json")
